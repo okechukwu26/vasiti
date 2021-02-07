9fbeb99df7e5a0b73fa9fd48cd033b57db987659
oke <priceblings@gmail.com>
2021-02-07 23:09:52 +0100

    node

--- src/middleware/global.ts
+++ src/middleware/global.ts
@@ -1,10 +1,12 @@
 import cors from 'cors'
+import {trimInput} from './trimInputs'
 import express, { Express } from "express";
 import logger from "morgan";
 import passport from 'passport'
 import {jwtStrategy} from './passport'
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
+import bodyParser from 'body-parser'
 
 export default (app: Express) => {
     app.enable("trust proxy");
@@ -18,9 +20,12 @@ export default (app: Express) => {
     
     app.use(cors({maxAge:1728000}));
         app.use(express.json())
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
    
  
     app.use(logger("dev"));
     passport.use("jwt", jwtStrategy);
+    app.use(trimInput)
     
 };
