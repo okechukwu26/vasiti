ca1d898bc4d5ba2a289894e743ff0f9133c0d101
oke <priceblings@gmail.com>
2021-02-09 22:01:13 +0100

    test

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,20 @@ app.listen(port, () => {
   return logger.info(`server is listening on ${port}`);
 });
 
+// {
+//   "type":"mysql",
+//   "host":"aws-test.c6jrsctnu0a8.eu-west-2.rds.amazonaws.com",
+//   "port":3306,
+//   "username":"admin",
+//   "password":"okechukwu26",
+//   "database":"Motor",
+//   "synchronize":false,
+//   "logging":false,
+//  "entities":["**/api/**/*Model.js"],
+//   "migrations":["src/migration/*.js"],
+//   "cli":{
+//       "migrationsDir":"src/migration"
+//   }
+// }
+
+
