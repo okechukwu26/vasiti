4073a8db45efd2d31852adc405ad3404d79dab2b
oke <priceblings@gmail.com>
2021-02-09 21:09:21 +0100

    procfile

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import app from './app';
-import { PORT } from './config';
+
 import { logger } from './utils/logger';
 
-const port = PORT || 5000;
+let port = process.env.PORT || 5000;
 
 app.listen(port, () => { 
 
