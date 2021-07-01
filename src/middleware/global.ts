0f47f90151c07f31d9bfb1588d20170ed23d7473
oke <priceblings@gmail.com>
2021-07-01 16:53:06 +0100

    rate

--- src/middleware/global.ts
+++ src/middleware/global.ts
@@ -13,7 +13,7 @@ export default (app: Express) => {
     app.use(
         rateLimit({
             windowMs: 15 * 60 * 1000, // 15 minutes
-            max: 100, // limit each IP to 100 requests per windowMs
+            max: 1000, // limit each IP to 100 requests per windowMs
         }),
     );
     app.use(helmet());
